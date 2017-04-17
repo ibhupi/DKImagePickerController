b23355fea46ea0b54c13a389451815f966a5f244
Bhupendra Singh <bhupendra.singh@origami.com>
2017-04-17 20:26:53 +0900

    Showing 3 cloumns layout and maintain same ratios as screen

--- DKImagePickerController/View/DKAssetGroupGridLayout.swift
+++ DKImagePickerController/View/DKAssetGroupGridLayout.swift
@@ -18,18 +18,21 @@ open class DKAssetGroupGridLayout: UICollectionViewFlowLayout {
 			minItemWidth = 100
 		}
 		
-		let interval: CGFloat = 1
+		let interval: CGFloat = 0
 		self.minimumInteritemSpacing = interval
 		self.minimumLineSpacing = interval
 		
 		let contentWidth = self.collectionView!.bounds.width
 		
-		let itemCount = Int(floor(contentWidth / minItemWidth))
+		let itemCount = 3//Int(floor(contentWidth / minItemWidth)) // 3 view layout
 		var itemWidth = (contentWidth - interval * (CGFloat(itemCount) - 1)) / CGFloat(itemCount)
 		let actualInterval = (contentWidth - CGFloat(itemCount) * itemWidth) / (CGFloat(itemCount) - 1)
 		itemWidth += actualInterval - interval
 		
-		let itemSize = CGSize(width: itemWidth, height: itemWidth)
+        let contentSize = self.collectionView!.bounds.size
+        let itemHeight = contentSize.height * (itemWidth / contentSize.width)
+        
+		let itemSize = CGSize(width: itemWidth, height: itemHeight)
 		self.itemSize = itemSize
 	}
 	
